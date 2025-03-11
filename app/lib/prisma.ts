8770cee133ec9ab9e7638a1df82fc54d87cc0fc8
Ryuki Tobita <t0gah1m1k0@kur1su-amad3us.tokyo>
2025-03-11 16:37:15 +0900

    [Fix] linting fix

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-export interface IPrismaClient extends PrismaClient {}
-
-export const prisma: IPrismaClient = new PrismaClient({
+export const prisma = new PrismaClient({
   datasourceUrl: process.env.DATABASE_URL,
   log: ["query", "info", "warn", "error"],
 });
